bb658793fd8165b4df0407f2a52911e79d0eb99c
Kaveri Menoki <kaveri@Kaveris-MacBook-Pro.local>
2023-01-16 16:47:59 +0530

    Added the contents of App.vue from the original app

--- src/App.vue
+++ src/App.vue
@@ -1,130 +1,74 @@
-<script setup>
-import { onMounted } from "vue";
-import { PDFDocument, StandardFonts, rgb } from "pdf-lib";
-import download from "downloadjs";
-import HelloWorld from "./components/HelloWorld.vue";
-
-// onMounted(async () => {
-//   const generatePdf = async () => {
-//     // Create a new PDFDocument
-//     const pdfDoc = await PDFDocument.create();
-//
-//     // Embed the Times Roman font
-//     const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
-//
-//     // Add a blank page to the document
-//     const page = pdfDoc.addPage();
-//
-//     // Get the width and height of the page
-//     const { width, height } = page.getSize();
-//
-//     // Draw a string of text toward the top of the page
-//     const fontSize = 30;
-//     page.drawText("Creating PDFs in JavaScript is awesome!", {
-//       x: 50,
-//       y: height - 4 * fontSize,
-//       size: fontSize,
-//       font: timesRomanFont,
-//       color: rgb(0, 0.53, 0.71),
-//     });
-//
-//     // Serialize the PDFDocument to bytes (a Uint8Array)
-//     const pdfBytes = await pdfDoc.save();
-//     download(pdfBytes, "pdf-lib_creation_example.pdf", "application/pdf");
-//   };
-//
-//   await generatePdf();
-// });
-</script>
-
 <template>
-  <div id="app">
-    <header>
-      <img
-        alt="Vue logo"
-        class="logo"
-        src="@/assets/logo.svg"
-        width="125"
-        height="125"
-      />
+  <div
+    id="app"
+    :dir="direction"
+  >
+    {{ $t('') }} <!-- Added intentionally as component does not re render on locale change if i18n library is not used in template -->
+    <notifications
+      group="custom-template"
+      :position="'bottom ' + notificationDirection"
+      :width="400">
+      <template
+        slot="body"
+        scope="props">
+        <div :class="'custom-template vue-notification ' + props.item.type ">
+          <div class="row">
+            <div class="custom-template-icon">
+              <i
+                class="fa fa-check-circle fa-3x"
+                v-if="props.item.type === 'success'"/>
+              <i
+                class="fa fa-times-circle fa-3x"
+                v-if="props.item.type === 'error'"/>
+              <i
+                class="fa fa-exclamation-circle fa-3x"
+                v-if="props.item.type === 'warn'"/>
+            </div>
+            <div class="custom-template-content">
+              <div class="custom-template-title header-bold">
+                {{ props.item.title }}
+              </div>
+              <div
+                class="custom-template-text sub-header"
+                v-html="props.item.text" />
+            </div>
+          </div>
+        </div>
+      </template>
+    </notifications>
 
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-
-        <nav>
-          <router-link to="/">Home</router-link>
-          <router-link to="/about">About</router-link>
-        </nav>
-      </div>
-    </header>
-
-    <router-view />
+    <div
+      class="loading"
+      id="loader">
+      <div class="error">{{ $t('mixed.loading') }}</div>
+    </div>
+    <layout/>
   </div>
 </template>
 
+<script>
+  import Layout from './components/layout/layout.vue';
+  import {LayoutDirectionConfig} from './plugins/i18n';
+
+  export default {
+    name: 'App',
+    components: {
+      Layout,
+    },
+    data() {
+      return {
+        direction: 'ltr',
+        notificationDirection: 'right',
+      };
+    },
+    updated() {
+      this.direction = LayoutDirectionConfig[this.$i18n.locale];
+      this.notificationDirection = this.direction === 'ltr'? 'right': 'left'
+    }
+
+  };
+</script>
+
 <style lang="scss">
-@import "./assets/stylesheets/main.scss";
+  @import '../src/assets/stylesheets/main.scss';
 </style>
-<!--<style scoped>-->
-<!--header {-->
-<!--  line-height: 1.5;-->
-<!--  max-height: 100vh;-->
-<!--}-->
-
-<!--.logo {-->
-<!--  display: block;-->
-<!--  margin: 0 auto 2rem;-->
-<!--}-->
-
-<!--nav {-->
-<!--  width: 100%;-->
-<!--  font-size: 12px;-->
-<!--  text-align: center;-->
-<!--  margin-top: 2rem;-->
-<!--}-->
-
-<!--nav a.router-link-exact-active {-->
-<!--  color: var(&#45;&#45;color-text);-->
-<!--}-->
-
-<!--nav a.router-link-exact-active:hover {-->
-<!--  background-color: transparent;-->
-<!--}-->
-
-<!--nav a {-->
-<!--  display: inline-block;-->
-<!--  padding: 0 1rem;-->
-<!--  border-left: 1px solid var(&#45;&#45;color-border);-->
-<!--}-->
-
-<!--nav a:first-of-type {-->
-<!--  border: 0;-->
-<!--}-->
-
-<!--@media (min-width: 1024px) {-->
-<!--  header {-->
-<!--    display: flex;-->
-<!--    place-items: center;-->
-<!--    padding-right: calc(var(&#45;&#45;section-gap) / 2);-->
-<!--  }-->
-
-<!--  .logo {-->
-<!--    margin: 0 2rem 0 0;-->
-<!--  }-->
-
-<!--  header .wrapper {-->
-<!--    display: flex;-->
-<!--    place-items: flex-start;-->
-<!--    flex-wrap: wrap;-->
-<!--  }-->
-
-<!--  nav {-->
-<!--    text-align: left;-->
-<!--    margin-left: -1rem;-->
-<!--    font-size: 1rem;-->
-
-<!--    padding: 1rem 0;-->
-<!--    margin-top: 1rem;-->
-<!--  }-->
-<!--}-->
-<!--</style>-->
