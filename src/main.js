a0b9ce04c656690f8d5a1a9c955f557f21314a42
Kaveri Menoki <kaveri@Kaveris-MacBook-Pro.local>
2023-01-16 16:47:03 +0530

    Integrated i18n to main.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from "vue";
 
 import App from "./App.vue";
 import router from "./router";
+import {i18n} from './plugins/i18n';
 
 import "./assets/stylesheets/main.scss";
 
 new Vue({
   router,
+  i18n,
   render: (h) => h(App),
 }).$mount("#app");
