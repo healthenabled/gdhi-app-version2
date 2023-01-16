c4b84641e6491d2c76ee16e389d922a53b926546
Kaveri Menoki <kaveri@Kaveris-MacBook-Pro.local>
2023-01-16 16:31:10 +0530

    Added plugins directory from the original app

---
+++ src/plugins/i18n.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+import {ar, en, es, fr, pt} from '../static-content/index';
+
+Vue.use(VueI18n);
+const messages = {
+  en,
+  es,
+  fr,
+  pt,
+  ar,
+};
+
+const english = 'en';
+export const i18n = new VueI18n({
+  locale: 'en',
+  fallbackLocale: english,
+  messages,
+});
+
+const LTR = 'ltr';
+const RTL = 'rtl';
+
+export const LayoutDirectionConfig = {
+  en: LTR,
+  es: LTR,
+  fr: LTR,
+  pt: LTR,
+  ar: RTL,
+};
