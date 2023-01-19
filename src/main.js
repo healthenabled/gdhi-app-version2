0bb7c45ae3ae3f336efebd1f7318ab3ab6869fc5
Kaveri Menoki <kaveri@Kaveris-MacBook-Pro.local>
2023-01-19 16:14:01 +0530

    Added the main.js file

--- src/main.js
+++ src/main.js
@@ -1,13 +1,28 @@
-import Vue from "vue";
-
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue';
+import Notifications from 'vue-notification';
+import App from './App.vue';
+import router from './router';
+// noinspection ES6UnusedImports
+import {} from './global.js';
 import {i18n} from './plugins/i18n';
+import VueCookies from 'vue-cookies';
 
-import "./assets/stylesheets/main.scss";
+Vue.use(VueCookies);
+VueCookies.config('7d');
 
+Vue.config.productionTip = false;
+
+Vue.use(Notifications);
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
   router,
   i18n,
   render: (h) => h(App),
+  components: {App, Notifications},
+  mounted() {
+    if (this.$route.fullPath.length <= 1) {
+      this.$router.push('map');
+    }
+  },
 }).$mount("#app");
