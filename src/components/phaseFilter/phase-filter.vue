b015e978fc7661f01f4b5b4f9f73a27615ddd4f1
Shaswat Prabhat <spd1238@gmail.com>
2023-02-13 11:45:27 +0530

    GDHI-17 Updated Styles for Phase filter and Indicator filter components <Shaswat>

---
+++ src/components/phaseFilter/phase-filter.vue
@@ -0,0 +1,62 @@
+<script>
+import Vue from "vue";
+import axios from "axios";
+import { EventBus } from "../common/event-bus";
+
+export default Vue.extend({
+  name: "phaseFilter",
+  data() {
+    return {
+      phaseValue: "",
+      phases: [],
+    };
+  },
+
+  created() {
+    this.phaseValue = window.appProperties.getPhaseFilter();
+    this.fetchPhases();
+  },
+
+  methods: {
+    filter: function () {
+      window.appProperties.setPhaseFilter({ phaseId: this.phaseValue });
+      EventBus.$emit("Map:filtered");
+      EventBus.$emit("Panel:filtered");
+    },
+
+    resetFilters: function () {
+      this.phaseValue = "";
+      this.filter();
+    },
+
+    fetchPhases: function () {
+      const self = this;
+      axios.get("/api/phases").then((response) => {
+        self.phases = response.data;
+      });
+    },
+  },
+});
+</script>
+<template>
+  <div class="phase-indicator">
+    <div class="phase-indicator-header">
+      {{ $t("mixed.phase") }}
+    </div>
+    <select
+      class="phase-indicator-select"
+      v-model="phaseValue"
+      @change="filter()"
+      name="test_select2"
+    >
+      <option value="">{{ $t("mixed.all") }}</option>
+      <option
+        v-for="(phase, index) in phases"
+        :key="index"
+        v-bind:value="phase.phaseValue"
+      >
+        {{ $t("mixed.phase") }} {{ phase.phaseValue }}
+      </option>
+    </select>
+  </div>
+</template>
