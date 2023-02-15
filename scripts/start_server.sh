128b36f09a36d77f554a4599d04f29aecb6a3596
ashutoshroy <ashutoshroywork@gmail.com>
2023-02-15 12:26:42 +0530

    GDHI-Tech : Update startup script <Ashutosh>

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,14 +3,16 @@ set -xe
 pwd
 source /opt/scripts/export_aws_vars.sh
 FILE=`/usr/local/bin/aws s3api list-objects-v2 --bucket "$AWS_S3_BUCKET_NAME" --query 'reverse(sort_by(Contents[?contains(Key, \`.jar\`)], &LastModified))[:1].Key' --output=text`;
-
+service httpd stop 
 /usr/local/bin/aws s3 cp "s3://$AWS_S3_BUCKET_NAME/$FILE"  /var/www/html
+
+
 if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Frontend-v2-QA" ]
 then 
-    echo "Execute for QA"
+    service httpd restart 
 fi    
 if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Frontend-v2-Showcase" ]
 then 
-    echo "Execute for Showcase"
+    service httpd restart 
 fi    
 
