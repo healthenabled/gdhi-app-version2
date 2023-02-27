067256211f0a5896e462267ef01fea97a6990152
ashutoshroy <ashutoshroywork@gmail.com>
2023-02-27 12:17:57 +0530

    GDHI-Tech : Update build.yml <Ashutosh>

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,14 +5,4 @@ source /opt/scripts/export_aws_vars.sh
 service httpd stop 
 FILE=`/usr/local/bin/aws s3 ls $AWS_S3_BUCKET_NAME/$AWS_S3_FOLDER_FRONTEND/ --recursive | sort | tail -n 1 | awk -F'/' '{print $2}'`
 /usr/local/bin/aws s3 cp "s3://$AWS_S3_BUCKET_NAME/$AWS_S3_FOLDER_FRONTEND/$FILE"  "/var/www/html/" --recursive
-
-if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Frontend-v2-QA" ]
-then 
-    service httpd restart 
-fi    
-if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Frontend-v2-Showcase" ]
-then 
-    service httpd restart 
-fi    
-
-# FILE="$(/usr/local/bin/aws s3 ls $AWS_S3_BUCKET_NAME --recursive | sort | tail -n 1 | awk '{print $4}')"
+service httpd restart 
