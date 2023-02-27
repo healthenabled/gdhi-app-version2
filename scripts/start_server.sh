502236a25e58a60a183b70742cdecadf9c5f5d27
ashutoshroy <ashutoshroywork@gmail.com>
2023-02-27 11:57:27 +0530

    GDHI-Tech : Copy latest versioned folder from S3 <Ashutosh>

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,8 +3,8 @@ set -xe
 pwd
 source /opt/scripts/export_aws_vars.sh
 service httpd stop 
-FILE=`/usr/local/bin/aws s3 ls $AWS_S3_BUCKET_NAME/$AWS_S3_FOLDER_FRONTEND/ --recursive | sort | tail -n 1 | awk '{print $4}'`
-/usr/local/bin/aws s3 cp "s3://$AWS_S3_BUCKET_NAME/$FILE"  "/var/www/html/" --recursive
+FILE=`/usr/local/bin/aws s3 ls $AWS_S3_BUCKET_NAME/$AWS_S3_FOLDER_FRONTEND/ --recursive | sort | tail -n 1 | awk -F'/' '{print $2}'`
+/usr/local/bin/aws s3 cp "s3://$AWS_S3_BUCKET_NAME/$AWS_S3_FOLDER_FRONTEND/$FILE"  "/var/www/html/" --recursive
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Frontend-v2-QA" ]
 then 
@@ -15,3 +15,4 @@ then
     service httpd restart 
 fi    
 
+# FILE="$(/usr/local/bin/aws s3 ls $AWS_S3_BUCKET_NAME --recursive | sort | tail -n 1 | awk '{print $4}')"
