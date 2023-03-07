e33c1e222d0a10b54ef15f673618fc87c6245c37
Shaswat Prabhat <spd1238@gmail.com>
2023-03-07 18:29:46 +0530

    GDHI-37: Fix color code test. <Shaswat>

--- src/components/__tests__/color-codes.spec.js
+++ src/components/__tests__/color-codes.spec.js
@@ -3,11 +3,11 @@ import colorCodes from "../common/color-codes";
 
 describe("color codes", () => {
   let colorObj = [
-    { score: "5", color: "#01C975", description: "Most Developed" },
-    { score: "4", color: "#80E1CC", description: "" },
-    { score: "3", color: "#FFE180", description: "" },
-    { score: "2", color: "#FFCA82", description: "" },
     { score: "1", color: "#FCAB9C", description: "Least Developed" },
+    { score: "2", color: "#FFCA82", description: "" },
+    { score: "3", color: "#FFE180", description: "" },
+    { score: "4", color: "#80E1CC", description: "" },
+    { score: "5", color: "#01C975", description: "Most Developed" },
     { score: "NA", color: "#6C757D" },
   ];
   it(" should return the color codes object", () => {
