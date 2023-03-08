51f5af2862c0ec7ddc71883d0c8bf810a79dff25
ashutoshroy <ashutoshroywork@gmail.com>
2023-03-08 14:42:29 +0530

    GDHI-35 : Add Year Selector and tests <Shaswat>

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,14 +3,6 @@ set -xe
 pwd
 source /opt/scripts/export_aws_vars.sh
 service httpd stop 
-/usr/local/bin/aws s3 cp "s3://$AWS_S3_BUCKET_NAME/$AWS_S3_FOLDER_FRONTEND/"  "/var/www/html/" --recursive
-
-if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Frontend-v2-QA" ]
-then 
-    service httpd restart 
-fi    
-if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Frontend-v2-Showcase" ]
-then 
-    service httpd restart 
-fi    
-
+FILE=`/usr/local/bin/aws s3 ls $AWS_S3_BUCKET_NAME/$AWS_S3_FOLDER_FRONTEND/ --recursive | sort | tail -n 1 | awk -F'/' '{print $2}'`
+/usr/local/bin/aws s3 cp "s3://$AWS_S3_BUCKET_NAME/$AWS_S3_FOLDER_FRONTEND/$FILE"  "/var/www/html/" --recursive
+service httpd restart 
