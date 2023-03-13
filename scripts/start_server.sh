39a02b3877bbd52cd8f565c446f28e9484e19146
ashutoshroy <ashutoshroywork@gmail.com>
2023-03-13 17:08:14 +0530

    GDHI-60 Update the folder logic for deployment to QA and Showcase . <Ashutosh>

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,25 +3,28 @@ set -xe
 pwd
 source /opt/scripts/export_aws_vars.sh
 service httpd stop 
-FILE=`/usr/local/bin/aws ssm get-parameters --name "buildNumberForApp" --query "Parameters[0].Value" | tr -d '"'`
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Backend-v2-QA" ]
+then
+    source /opt/scripts/export_QA_env.sh
+    FILE=`/usr/local/bin/aws s3 ls $AWS_S3_BUCKET_NAME/$AWS_S3_FOLDER_FRONTEND/ --recursive | sort | tail -n 1 | awk -F'/' '{print $2}'`
+fi
+if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Backend-v2-Showcase" ]
+then
+    source /opt/scripts/export_Showcase_env.sh
+    FILE=`/usr/local/bin/aws ssm get-parameters --name "buildNumberForApp" --query "Parameters[0].Value" | tr -d '"'`
+fi
+# if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Backend-v2-Prod" ]
+# then
+#     source /opt/scripts/export_Prod_env.sh
+#     FILE=`/usr/local/bin/aws ssm get-parameters --name "buildNumberForApp" --query "Parameters[0].Value" | tr -d '"'`
+# fi
+
 /usr/local/bin/aws s3 cp "s3://$AWS_S3_BUCKET_NAME/$AWS_S3_FOLDER_FRONTEND/$FILE"  "/var/www/html/" --recursive
 service httpd restart 
 
 # ---------------------------
 # Delete the data from QA RDS
 # ---------------------------
-
-if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Backend-v2-QA" ]
-then
-    source /opt/scripts/export_QA_env.sh
-fi
-if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Backend-v2-Showcase" ]
-then
-    source /opt/scripts/export_Showcase_env.sh
-fi
-# if [ "$DEPLOYMENT_GROUP_NAME" == "GDHI-Backend-v2-Prod" ]
-# then
-#     source /opt/scripts/export_Prod_env.sh
-# fi
 # psql postgresql://$DB_USERNAME:$DB_PASSWORD@$DB_HOST/$DB__SCHEMA_NAME -f $SQL_FILE_PATH
 # echo -n "Delete data from QA RDS executing "+$SQL_FILE_PATH
