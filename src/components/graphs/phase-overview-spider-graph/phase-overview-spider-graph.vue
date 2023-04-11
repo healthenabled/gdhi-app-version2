5577b2dc8fb8abc574de7327be0528cc7bee60c7
Shaswat Prabhat <spd1238@gmail.com>
2023-04-11 09:55:52 +0530

    GDHI-75: Add Updated method to reflect when data is getting updated <Shaswat>

--- src/components/graphs/phase-overview-spider-graph/phase-overview-spider-graph.vue
+++ src/components/graphs/phase-overview-spider-graph/phase-overview-spider-graph.vue
@@ -132,6 +132,56 @@ export default {
       },
     });
   },
+  updated() {
+    Chart.defaults.font.size = 16;
+    Chart.defaults.font.family = "'InterRegular', sans-serif";
+    new Chart(document.getElementById("phase-overview-spider-graph"), {
+      type: "radar",
+      data: this.graphData,
+      options: {
+        elements: {
+          line: {
+            borderWidth: 1,
+          },
+        },
+        scales: {
+          r: {
+            angleLines: {
+              display: true,
+            },
+            suggestedMin: 0,
+            alignToPixels: true,
+            beginAtZero: true,
+            ticks: {
+              count: 5,
+              format: "",
+            },
+            pointLabels: {
+              font: {
+                size: 9,
+                family: "'InterRegular', sans-serif",
+              },
+              backdropPadding: 0,
+              padding: 2,
+              callback: (args) => {
+                return args.replaceAll(" ", "\n");
+              },
+            },
+          },
+        },
+        layout: {
+          autoPadding: false,
+          padding: 0,
+        },
+        plugins: {
+          legend: {
+            display: true,
+            position: "bottom",
+          },
+        },
+      },
+    });
+  },
 };
 </script>
 
