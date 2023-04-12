2e2b54eada687d010e98a8ea693fa8a2097999ee
haikyuu77 <arjunsure.mec@gmail.com>
2023-04-12 15:02:52 +0530

    GDHI-111: Add LineChart for year on year progression <Arjun>

---
+++ src/components/lineGraph/lineGraphChart.vue
@@ -0,0 +1,263 @@
+<template>
+  <canvas id="myChart" dir="rtl"></canvas>
+</template>
+<script>
+import Chart from "chart.js/auto";
+import annotationPlugin from "chartjs-plugin-annotation";
+import { i18n, LayoutDirectionConfig } from "../../plugins/i18n";
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "LineGraphChart",
+  data() {
+    return {
+      labels: ["", "Version1", "2023", "2024"],
+      chart: null,
+    };
+  },
+  props: {
+    yearOnYearData: { type: Array, required: true },
+    locale: { type: String, required: true },
+  },
+
+  computed: {
+    countryData() {
+      let val = [];
+      let yearPhaseMap = new Map();
+      this.yearOnYearData.yearOnYearData.map((element) => {
+        yearPhaseMap.set(element.year, element.data.country.countryPhase);
+      });
+      this.labels.map((label) => {
+        if (yearPhaseMap.has(label)) {
+          val.push(yearPhaseMap.get(label));
+        } else {
+          val.push(null);
+        }
+      });
+      return val;
+    },
+
+    globalData() {
+      let val = [];
+      let yearPhaseMap = new Map();
+      this.yearOnYearData.yearOnYearData.map((element) => {
+        yearPhaseMap.set(element.year, element.data.average.overAllScore);
+      });
+      this.labels.map((label) => {
+        if (yearPhaseMap.has(label)) {
+          val.push(yearPhaseMap.get(label));
+        } else {
+          val.push(null);
+        }
+      });
+      return val;
+    },
+
+    lineChartData() {
+      return {
+        labels: this.labels,
+        datasets: [
+          {
+            label:
+              this.yearOnYearData.yearOnYearData[0].data.country.countryName,
+            data: this.countryData,
+            fill: false,
+            borderColor: "black",
+            spanGaps: true,
+            pointBorderColor: "rgba(0,0,0,0)",
+            pointStyle: "rectRot",
+            borderWidth: 1.5,
+            pointRadius: 14,
+            pointHoverRadius: 16,
+            pointBackgroundColor: function (context) {
+              if (context.parsed.y === 1) {
+                return "red";
+              } else if (context.parsed.y === 2) {
+                return "orange";
+              } else if (context.parsed.y === 3) {
+                return "yellow";
+              } else if (context.parsed.y === 4) {
+                return "green";
+              } else if (context.parsed.y === 5) {
+                return "darkGreen";
+              } else {
+                return "silver";
+              }
+            },
+
+            segment: {
+              borderColor: (ctx) => {
+                const skipped = (ctx, value) =>
+                  ctx.p0.skip || ctx.p1.skip ? value : undefined;
+                const down = (ctx, value) =>
+                  ctx.p0.parsed.y > ctx.p1.parsed.y ? value : undefined;
+                skipped(ctx, "rgb(0,0,0,0.2)") || down(ctx, "rgb(192,75,75)");
+              },
+              borderDash: (ctx) => {
+                const skipped = (ctx, value) =>
+                  ctx.p0.skip || ctx.p1.skip ? value : undefined;
+                const down = (ctx, value) =>
+                  ctx.p0.parsed.y > ctx.p1.parsed.y ? value : undefined;
+                skipped(ctx, [4, 4]);
+              },
+            },
+          },
+          {
+            label: "Global average",
+            data: this.globalData,
+            fill: false,
+            borderColor: "grey",
+            tension: 0.1,
+            borderDash: [5, 4],
+            borderWidth: 1.5,
+            pointStyle: false,
+            pointRadius: 14,
+            spanGaps: true,
+          },
+        ],
+      };
+    },
+
+    lineChartOptions() {
+      return {
+        clip: false,
+        layout: {
+          padding: {
+            left: 20,
+            right: 80,
+            top: 60,
+            bottom: 60,
+          },
+        },
+        animation: false,
+        plugins: {
+          tooltip: {
+            rtl: LayoutDirectionConfig[i18n.locale] === "rtl",
+            callbacks: {
+              label: function (context) {
+                let label = context.dataset.label || "";
+
+                if (label) {
+                  label += " ::Phase: ";
+                }
+                if (context.parsed.y !== null) {
+                  label += context.parsed.y;
+                }
+                return label;
+              },
+            },
+          },
+          legend: {
+            rtl: LayoutDirectionConfig[i18n.locale] === "rtl",
+            position: "bottom",
+            labels: {
+              boxHeight: 0,
+            },
+          },
+          annotation: {
+            annotations: {
+              line: {
+                type: "line",
+                xMin: this.yearOnYearData.currentYear,
+                xMax: this.yearOnYearData.currentYear,
+                backgroundColor: "yellow",
+                borderColor: "darkblue",
+                borderWidth: 1.5,
+                borderDash: [20, 16],
+                drawTime: "beforeDatasetsDraw",
+              },
+            },
+          },
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            position:
+              LayoutDirectionConfig[i18n.locale] === "rtl" ? "right" : "left",
+            grid: {
+              borderDash: [4, 4],
+            },
+            min: 0,
+            max: 5,
+            ticks: {
+              stepSize: 1,
+              beginAtZero: false,
+              callback: function (value, index, values) {
+                const labels = [
+                  "",
+                  "Phase 1",
+                  "Phase 2",
+                  "Phase 3",
+                  "Phase 4",
+                  "Phase 5",
+                ];
+                return labels[value];
+              },
+            },
+          },
+          x: {
+            reverse: LayoutDirectionConfig[i18n.locale] === "rtl",
+            grid: {
+              display: false,
+            },
+            ticks: {
+              beginAtZero: true,
+              stepSize: 1,
+              min: 1,
+            },
+          },
+        },
+      };
+    },
+
+    lineChartConfig() {
+      return {
+        type: "line",
+        data: this.lineChartData,
+        options: this.lineChartOptions,
+      };
+    },
+  },
+
+  mounted() {
+    this.drawLineChart();
+  },
+
+  // updated() {
+  //   console.log("updated invoked");
+  //   if (this.locale != i18n.locale) {
+  //     this.locale = i18n.locale;
+  //   }
+  // },
+
+  watch: {
+    yearOnYearData() {
+      console.log("Year Data changed");
+      this.chart.destroy();
+      this.drawChart();
+    },
+
+    locale() {
+      console.log("Language change detected");
+      this.chart.destroy();
+      this.drawChart();
+    },
+  },
+
+  methods: {
+    drawLineChart() {
+      Chart.register(annotationPlugin);
+      this.chart = new Chart(
+        document.getElementById("myChart"),
+        this.lineChartConfig
+      );
+    },
+    getLabels() {
+      this.yearOnYearData.yearOnYearData.map((element) => {
+        this.labels.push(element.year);
+      });
+    },
+  },
+});
+</script>
