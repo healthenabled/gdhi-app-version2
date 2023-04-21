e7ce72a19b24fecb2768f7e55bfbb35a0f404174
Shaswat Prabhat <spd1238@gmail.com>
2023-04-21 13:40:47 +0530

    GDHI-88: Fix graph destroy error on Browser <Shaswat>

--- src/components/__tests__/phase-overview-spider-graph.spec.js
+++ src/components/__tests__/phase-overview-spider-graph.spec.js
@@ -28,7 +28,7 @@ describe("Phase OverView Spider Graph", () => {
   it("should render Graph when mounted and create a new Chart instance", () => {
     expect(wrapper.vm.$el).toMatchSnapshot();
     vi.useFakeTimers();
-    vi.advanceTimersByTime(500);
+    vi.advanceTimersByTime(700);
     expect(Chart.mock.calls.length).toEqual(1);
     expect(Chart.mock.calls[0][1]).toMatchSnapshot();
     // TODO: Fix mock of ChartJS to be able to assert watch call
