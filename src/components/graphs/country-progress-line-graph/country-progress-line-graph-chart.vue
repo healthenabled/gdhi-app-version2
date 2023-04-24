9c236ff1b47a773d783e7d1c5444aed9c0025656
laharigundu2001 <lahari.gundu@thoughtworks.com>
2023-04-24 18:33:12 +0530

    GDHI-91: Add region name in computed <Lahari/Ashutosh>

--- src/components/graphs/country-progress-line-graph/country-progress-line-graph-chart.vue
+++ src/components/graphs/country-progress-line-graph/country-progress-line-graph-chart.vue
@@ -36,6 +36,7 @@ export default Vue.extend({
         xAxisLabels: this.xAxisLabels,
         countryName: this.countryName,
         averageText: this.averageText,
+        regionName: this.regionName,
       };
     },
     averageText() {
