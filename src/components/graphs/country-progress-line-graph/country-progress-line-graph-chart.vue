45d15eedcd3a1c8cc529cc29bd1d1449d62186dd
haikyuu77 <arjunsure.mec@gmail.com>
2023-04-25 10:13:45 +0530

    GDHI-88 Update global data field for line chart <Arjun>

--- src/components/graphs/country-progress-line-graph/country-progress-line-graph-chart.vue
+++ src/components/graphs/country-progress-line-graph/country-progress-line-graph-chart.vue
@@ -78,9 +78,19 @@ export default Vue.extend({
       let val = [];
       let yearPhaseMap = new Map();
       this.yearOnYearData.map(({ data, year }) => {
-        if (data.average.overAllScore === -1) yearPhaseMap.set(year, 0);
-        else {
-          yearPhaseMap.set(year, data.average.overAllScore);
+        if (this.categoryFilter < 0 && data.average.categories) {
+          if (data.average.overAllScore === -1) yearPhaseMap.set(year, 0);
+          else {
+            yearPhaseMap.set(year, data.average.overAllScore);
+          }
+        } else if (data.average.categories) {
+          const categoryPhaseValue =
+            data.average.categories[this.categoryFilter].phase;
+          if (categoryPhaseValue >= 0)
+            yearPhaseMap.set(year, categoryPhaseValue);
+          else if (categoryPhaseValue === -1) {
+            yearPhaseMap.set(year, 0);
+          }
         }
       });
       this.xAxisLabels.map((label) => {
