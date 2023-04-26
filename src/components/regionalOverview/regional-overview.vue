d82236d73f9e1a9a04efad0148dd361242d7d565
ashutoshroy <ashutoshroywork@gmail.com>
2023-04-26 14:35:44 +0530

    GDHI-83 Create regional overview page <Ashutosh/Kaveri>

---
+++ src/components/regionalOverview/regional-overview.vue
@@ -0,0 +1,25 @@
+<script>
+import Vue from "vue";
+import { EventBus } from "../common/event-bus";
+import { EVENTS } from "../../constants";
+import { LayoutDirectionConfig } from "../../plugins/i18n";
+
+export default Vue.extend({
+  name: "RegionalOverview",
+  data() {
+    return {
+      regions: [],
+      selectedRegion: "",
+    };
+  },
+});
+</script>
+<template>
+  <div>
+    <h3>{{ $route.params.regionName }}</h3>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "regional-overview";
+</style>
