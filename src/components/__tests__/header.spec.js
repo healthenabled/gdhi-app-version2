f73cb093af819a03f0b0b45baabad007f2706b3e
ashutoshroy <ashutoshroywork@gmail.com>
2023-04-27 12:01:35 +0530

    GDHI-83 Fix unhandled exception in test case <Ashutosh/Kaveri>

--- src/components/__tests__/header.spec.js
+++ src/components/__tests__/header.spec.js
@@ -1,4 +1,5 @@
-import { shallowMount, mount, RouterLinkStub } from "@vue/test-utils";
+import { mount, RouterLinkStub } from "@vue/test-utils";
+import VueRouter from "vue-router";
 import { describe, expect, it, vi } from "vitest";
 import Header from "../header/header.vue";
 import { i18n } from "../../plugins/i18n";
@@ -6,7 +7,7 @@ import axios from "axios";
 
 describe("Header ", () => {
   let wrapper;
-
+  const router = new VueRouter();
   let countryData = [
     { id: "IND", name: "India", countryAlpha2Code: "IN" },
     { id: "USA", name: "United States of America", countryAlpha2Code: "US" },
@@ -17,19 +18,15 @@ describe("Header ", () => {
   axiosGetSpy.mockResolvedValue({ data: countryData });
 
   it("should have the data", () => {
-    wrapper = shallowMount(Header, { i18n });
-    expect(wrapper.vm.countries).to.deep.equal({});
-    expect(wrapper.vm.developmentIndicators).to.deep.equal([]);
-    expect(wrapper.vm.healthIndicators).to.deep.equal({});
-  });
-
-  it("should have the data", () => {
     wrapper = mount(Header, {
       stubs: {
         "router-link": RouterLinkStub,
       },
       i18n,
+      router,
     });
-    expect(wrapper.findAll(".hd-element").length).to.equal(4);
+    const mockPush = vi.fn();
+    wrapper.vm.$router = { push: mockPush };
+    expect(wrapper.findAll(".hd-element").length).to.equal(5);
   });
 });
