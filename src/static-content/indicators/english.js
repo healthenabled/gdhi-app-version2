3487265a9289b17ab463dcd160082a4094947281
laharigundu2001 <lahari.gundu@thoughtworks.com>
2023-05-02 14:38:31 +0530

    GDHI-112 Update url and GDHI to GDHM in indicator page guide section <Lahari>

--- src/static-content/indicators/english.js
+++ src/static-content/indicators/english.js
@@ -2,8 +2,8 @@ export const indicators = {
   gdhi: "Global Digital Health Monitor",
   indicatorGuide: "Indicator Guide",
   paragraph1:
-    "The <a class='link-blue' href='http://digitalhealthindex.org' target='_blank'>Global Digital Health Index (GDHI)</a> is an " +
-    "interactive digital resource that tracks, monitors, and evaluates the use of digital technology for health across countries. GDHI uses " +
+    "The <a class='link-blue' href='http://digitalhealthmonitor.org' target='_blank'>Global Digital Health Monitor (GDHM)</a> is an " +
+    "interactive digital resource that tracks, monitors, and evaluates the use of digital technology for health across countries. GDHM uses " +
     "seven key indicator categories to track progress.",
   indicator: "Indicator",
 };
--- src/static-content/indicators/spanish.js
+++ src/static-content/indicators/spanish.js
@@ -2,8 +2,8 @@ export const indicators = {
   gdhi: "Monitor de salud digital mundial",
   indicatorGuide: "Guía de indicadores",
   paragraph1:
-    "El <a class='link-blue' href='http://digitalhealthindex.org' target='_blank'>Índice de Salud Digital Global (GDHI)</a> es un " +
-    "recurso digital interactivo que rastrea, monitorea y evalúa el uso de la tecnología digital para la salud en todos los países. El GDHI " +
+    "El <a class='link-blue' href='http://digitalhealthmonitor.org' target='_blank'>Monitor de salud digital mundial (GDHM)</a> es un " +
+    "recurso digital interactivo que rastrea, monitorea y evalúa el uso de la tecnología digital para la salud en todos los países. El GDHM " +
     "utiliza siete categorías de indicadores clave para seguir el progreso.",
   indicator: "Indicador",
 };
