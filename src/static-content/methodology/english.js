7e0e60a56c1603139d235281f5aa551cba432660
laharigundu2001 <lahari.gundu@thoughtworks.com>
2023-05-02 15:12:16 +0530

    GDHI-112 Update methodology <Lahari>

--- src/static-content/methodology/english.js
+++ src/static-content/methodology/english.js
@@ -2,38 +2,38 @@ export const methodology = {
   text: "Methodology",
   title1: "Our Approach",
   paragraph1:
-    "Based on the framework set in the World Health Organization – International Telecommunication Union’s 2012\n" +
-    "        National eHealth Strategy Toolkit, the GDHI team conducted rigorous research and engaged digital health\n" +
-    "        experts from over 20 countries to develop a recommended set of indicators that help countries track, monitor,\n" +
-    "        assess, and benchmark the effective use of digital health.",
+    "In early 2016, the GDHM team, together with representatives from 20+ countries and 50+ international agencies and organizations, designed version 1.0 of the Global Digital Health Index (GDHI).\n" +
+    "        In 2022, the resource underwent a year-long indicator and platform review and redesign process to align the indicators with the WHO Global Digital Health Strategy as a complement to the WHO Digital Health Atlas and includes a greater focus on AI,\n" +
+    "        equity, gender, and Universal Health Coverage. In addition, the name was changed to the Global Digital Health Monitor (GDHM) to articulate the tool’s value in providing a platform to monitor digital health progress at a country, regional and global level.\n" +
+    "        The updated GDHM includes year on year performance monitoring country visualizations, regional visualizations, and country to country comparisons.",
   bulletin: {
-    title: "To develop the indicators, the GDHI team:",
+    title: "To develop the indicators, the GDHM team:",
     line1:
       "Conducted a landscape review of global data available through multilateral organizations.",
     line2:
       "Mapped available data by reviewing existing frameworks and tools developed by experts to help practitioners\n" +
       "     and countries assess the strength and maturity of the enabling environment relevant to digital health.",
     line3:
-      "Convened digital health experts to develop use cases for GDHI and refine and define an initial set\n" +
+      "Convened digital health experts to develop use cases for GDHM and refine and define an initial set\n" +
       "     of recommended indicators into a user-friendly and manageable list.",
   },
-  paragraph2: "List of GDHI Indicators - ",
+  paragraph2: "List of GDHM Indicators - ",
   seeHere: "See here",
 
   title2: "Data Collection and Verification",
   paragraph3Start:
-    "The GDHI team works with country partners and Ministry of Health representatives who are\n" +
+    "The GDHM team works with country partners and Ministry of Health representatives who are\n" +
     "        leading digital health efforts in their respective countries to collect data for each country.\n" +
-    "        These country partners submit their country’s data through an annual GDHI survey,\n" +
+    "        These country partners submit their country’s data through an annual GDHM survey,\n" +
     "        and select the appropriate phase for each indicator as well as rationale and evidence to support these phases.\n" +
-    "        The GDHI team verifies the collected data before publishing it. Broader Health Data from the",
+    "        The GDHM team verifies the collected data before publishing it. Broader Health Data from the",
   paragraph3Link: "World Bank Development Indicators 2016",
   paragraph3End: "has been included to contextualize digital health maturity.",
 
   title3: "Calculating Averages and Benchmarking",
   paragraph4:
-    "The GDHI uses the main indicators in each category to calculate the overall country average.\n" +
-    "        While sub-indicators are included to add greater specificity to specific GDHI indicators,\n" +
+    "The GDHM uses the main indicators in each category to calculate the overall country average.\n" +
+    "        While sub-indicators are included to add greater specificity to specific GDHM indicators,\n" +
     "        they are not used in calculating the Component Phase or the Country or Global Averages.\n" +
-    "        The GDHI enables countries to benchmark themselves against the Global Average or a specific overall phase.",
+    "        The GDHM enables countries to benchmark themselves against the Global Average or a specific overall phase.",
 };
--- src/static-content/methodology/portuguese.js
+++ src/static-content/methodology/portuguese.js
@@ -2,36 +2,37 @@ export const methodology = {
   text: "Metodología",
   title1: "Nossa Abordagem",
   paragraph1:
-    "Com base na estrutura definida pela Organização Mundial de Saúde - União Internacional de Telecomunicações - Estratégia Nacional " +
-    "de eSaúde de 2012, a equipa GDHI realizou uma investigação rigorosa e envolveu peritos em saúde digital de mais de 20 países para desenvolver " +
-    "um conjunto de indicadores recomendados que ajudam os países a acompanhar, monitorizar, avaliar e a comparar a utilização eficaz da saúde digital.",
+    "No início de 2016, a equipe GDHM, juntamente com representantes de mais de 20 países e mais de 50 agências e organizações internacionais, projetou a versão 1.0 do Global Digital Health Index (GDHI)." +
+    " Em 2022, o recurso passou por um processo de revisão e redesenho de indicador e plataforma de um ano para alinhar os indicadores com a Estratégia Global de Saúde Digital da OMS como um complemento ao Atlas de Saúde Digital da OMS e inclui um foco maior em IA, " +
+    "equidade, gênero e Cobertura Universal de Saúde. Além disso, o nome foi alterado para Global Digital Health Monitor (GDHM) para articular o valor da ferramenta em fornecer uma plataforma para monitorar o progresso da saúde digital em nível nacional, regional e global." +
+    " O GDHM atualizado inclui visualizações de país de monitoramento de desempenho ano a ano, visualizações regionais e comparações de país para país.",
   bulletin: {
-    title: "Para desenvolver os indicadores, a equipa GDHI:",
+    title: "Para desenvolver os indicadores, a equipa GDHM:",
     line1:
       "Realizou uma revisão geral dos dados globais disponíveis através de organizações multilaterais.",
     line2:
       "Mapeou os dados disponíveis através da revisão dos quadros e ferramentas existentes desenvolvidos por especialistas para ajudar os " +
       "profissionais e os países a avaliar a força e a maturidade de um ambiente favorável e relevante para a saúde digital.",
     line3:
-      "Convocou especialistas em saúde digital para desenvolver casos de uso para o GDHI refinar e definir um conjunto inicial de indicadores " +
+      "Convocou especialistas em saúde digital para desenvolver casos de uso para o GDHM refinar e definir um conjunto inicial de indicadores " +
       "recomendados, vertidos numa lista de fácil gestão e utilização.",
   },
-  paragraph2: "Lista de Indicadores GDHI - ",
+  paragraph2: "Lista de Indicadores GDHM - ",
   seeHere: "Veja aqui",
 
   title2: "Recolha e verificação de dados",
   paragraph3Start:
-    "A equipe GDHI trabalha com parceiros nacionais e representantes do Ministério da Saúde que estão a liderar os esforços " +
+    "A equipe GDHM trabalha com parceiros nacionais e representantes do Ministério da Saúde que estão a liderar os esforços " +
     "de saúde digital nos seus respectivos países para reunir dados para cada país. Estes parceiros nacionais submetem os dados do seu país " +
-    "através de um inquérito anual GDHI e selecionam a fase apropriada para cada indicador, bem como a fundamentação e evidência para apoiar " +
-    "estas fases. A equipe GDHI verifica os dados reunidos antes de os publicar. Os dados mais amplos de saúde do ",
+    "através de um inquérito anual GDHM e selecionam a fase apropriada para cada indicador, bem como a fundamentação e evidência para apoiar " +
+    "estas fases. A equipe GDHM verifica os dados reunidos antes de os publicar. Os dados mais amplos de saúde do ",
   paragraph3Link: "Indicadores de Desenvolvimento do Banco Mundial 2016",
   paragraph3End:
     "foram incluídos para contextualizar a maturidade em saúde digital.",
 
   title3: "Cálculo de médias e benchmarking",
   paragraph4:
-    "O GDHI utiliza os principais indicadores em cada categoria para calcular a média geral do país. Embora os subindicadores sejam " +
-    "incluídos para adicionar maior especificidade a indicadores GDHI específicos, estes não são usados no cálculo da Fase Componente ou das Médias " +
-    "Globais ou do País. O GDHI permite que os países se comparem com a Média Global ou com uma fase geral específica.",
+    "O GDHM utiliza os principais indicadores em cada categoria para calcular a média geral do país. Embora os subindicadores sejam " +
+    "incluídos para adicionar maior especificidade a indicadores GDHM específicos, estes não são usados no cálculo da Fase Componente ou das Médias " +
+    "Globais ou do País. O GDHM permite que os países se comparem com a Média Global ou com uma fase geral específica.",
 };
