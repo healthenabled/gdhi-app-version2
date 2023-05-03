20b1c3de0bbcb31ada71b88fe699eeeade2af6fc
laharigundu2001 <lahari.gundu@thoughtworks.com>
2023-05-03 15:08:24 +0530

    GDHI-112 Update GDHI to GDHM in country_list <Lahari>

--- src/static-content/country-list/portuguese.js
+++ src/static-content/country-list/portuguese.js
@@ -1,7 +1,7 @@
 export const countryList = {
   title: "Lista de Países",
   line1:
-    "As fases gerais do GDHI estão enumeradas abaixo, para cada país participante. As pontuações dos países nos indicadores determinam " +
+    "As fases gerais do GDHM estão enumeradas abaixo, para cada país participante. As pontuações dos países nos indicadores determinam " +
     "em que fase da saúde digital um país se encontra.",
   line2:
     "Para uma análise mais detalhada de cada país, visite a página indvidual de cada país.",
--- src/static-content/country-list/spanish.js
+++ src/static-content/country-list/spanish.js
@@ -1,7 +1,7 @@
 export const countryList = {
   title: "Lista de países",
   line1:
-    "Las fases generales del GDHI se enumeran a continuación para cada país participante. Las puntuaciones de los países sobre los indicadores determinan la fase general de la salud digital de un país.",
+    "Las fases generales del GDHM se enumeran a continuación para cada país participante. Las puntuaciones de los países sobre los indicadores determinan la fase general de la salud digital de un país.",
   line2:
     "Para un desglose más detallado de cada país, visite la página de cada país",
   exportButtonText: "EXPORTACIÓN DE DATOS PARA TODOS LOS PAÍSES",
