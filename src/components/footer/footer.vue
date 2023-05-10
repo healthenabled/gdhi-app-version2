800c1f2e32ec928d4e264b50e71d0727b5fcf396
haikyuu77 <arjunsure.mec@gmail.com>
2023-05-10 16:28:14 +0530

    GDHI-131 Add styling to home page to make it mobile compatible <Arjun/Lahari>

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -3,7 +3,11 @@
     <div class="footer-width">
       <span>
         <span class="ft-element sub-header contact">
-          <span class="">{{ $t("footer.contactEmail") }}</span>
+          <span class="contact-info">{{ $t("footer.contactEmail") }}</span>
+          <span class="website-info"
+            >This website is best viewed on Desktops and Tablets and supports
+            multiple languages</span
+          >
         </span>
       </span>
     </div>
